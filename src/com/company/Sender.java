5f69c3c75affcafbe9edf5a438458f660d3ef15c
Osman Burak Intisah <intisah@geradesoftware.com>
2020-12-03 15:28:29 +0300

    initial commit

---
+++ src/com/company/Sender.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.io.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.*;
+
+public class Sender {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
